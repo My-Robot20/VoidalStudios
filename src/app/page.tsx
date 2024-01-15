a5282ed428e573df35157047680207605680e277
Daniel Braimah <danbrasafe@gmail.com>
2024-01-14 22:53:04 -0500

    First TailWind Integration success

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,17 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main>
-        <h1> Hello Next.J </h1>
-         <Link href="/about">Test Route</Link>
-    </main>
+    <div>
+      <h1 className='text-3xl text-yellow-400 font-bold font-sans md:font-serif'>
+        Voidal Studios
+      </h1>
+      <br></br>
+      <button className='rounded-md bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ...'>
+        Save changes
+      </button>
+
+      <br></br>
+      <Link href='/about'>Test Route</Link>
+    </div>
   )
 }
